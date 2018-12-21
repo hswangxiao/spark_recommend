09ca5c7154a5dc59e10ae1e6ad08517786b281e7
hswangxiao <wx459443845@qq.com>
2018-12-21 17:28:26 +0800

    git test

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -6,6 +6,6 @@
  */
 public class Test {
     public static void main(String[] args) {
-        System.out.println("create program test");
+//        System.out.println("create program test");
     }
 }
