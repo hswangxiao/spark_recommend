40f6a96514707f9a0b21db653f4cd50eeccd5283
hswangxiao <wx459443845@qq.com>
2018-12-21 17:21:25 +0800

    git test

---
+++ src/main/java/Test.java
@@ -0,0 +1,11 @@
+/**
+ * describe:
+ *
+ * @author wangxiao
+ * @date 2018/12/21
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("create program test");
+    }
+}
